c0d9d62ce1004664387671b31f30e85a9f46c3ee
AJAY KANDPAL <ajay.kandpal1998@gmail.com>
2024-03-11 17:35:43 +0530

    utils-code

---
+++ src/utils/AppError.js
@@ -0,0 +1,22 @@
+class AppError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null; //read it
+    this.message = message;
+    this.success = false; //as handlimg only error cases
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+export { AppError };
